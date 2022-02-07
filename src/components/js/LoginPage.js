fd26113d70628b9568418ffe281e568c187ce6d3
divyanshu-kumar123 <imcalleddevyanshu@gmail.com>
2022-02-07 16:13:06 +0530

    commit by aviral

--- src/components/js/LoginPage.js
+++ src/components/js/LoginPage.js
@@ -1,17 +1,17 @@
-const signUpButton = document.getElementById('signUp');
+// const signUpButton = document.getElementById('signUp');
 
-const signInButton = document.getElementById('signIn');
+// const signInButton = document.getElementById('signIn');
 
-const container = document.getElementById('container');
-
-
-
-signUpButton.addEventListener('click', () =>
-
-container.classList.add('right-panel-active'));
+// const container = document.getElementById('container');
 
 
 
-signInButton.addEventListener('click', () =>
+// signUpButton.addEventListener('click', () =>
 
-container.classList.remove('right-panel-active'));
+// container.classList.add('right-panel-active'));
+
+
+
+// signInButton.addEventListener('click', () =>
+
+// container.classList.remove('right-panel-active'));
